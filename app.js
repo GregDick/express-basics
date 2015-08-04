c4e386f52ec60adbf64957146d1989c8ba15811c
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-08-04 11:57:52 -0500

    added chicken nuggets form

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var fs = require('fs');
 //var app = require('express')();
 var express = require('express');
 var app = express();
-// var morgan = require('morgan');
-var loggly = require('loggly');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
 
 var routes = require('./routes/index');
 var pizza = require('./routes/pizza');
+var chickennuggets = require('./routes/chickennuggets');
 
 app.set('view engine', 'ejs');
 app.set('case sensitive routing', true);
@@ -16,39 +18,34 @@ app.set('strict routing', true);
 //all templates now have access to title
 app.locals.title = 'My Awesome App';
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('less-middleware')('public'));
 
-// var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
 
-// app.use(morgan('combined', {stream: logStream}));
-// app.use(morgan('dev'));
-
-
-var client = loggly.createClient({
-  token: 'f05cf77d-a401-4704-8a65-7574f901db83',
-  subdomain: 'gregdick',
-  tags: ['NodeJS'],
-  json: true
-})
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
 
 app.use(function(err, req, res, next){
-  client.log({ip: req.ip,
-  date: new Date(),
-  url: req.url,
-  status: res.statusCode,
-  method: req.method,
-  err: err
+  var client = require('./lib/loggly')('incoming');
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method
   });
   next();
 })
 
-
-
 app.use(express.static('public'));
+
+
 
 //========routes======//
 app.use('/', routes);
 app.use('/pizza', pizza);
+app.use('/chickennuggets', chickennuggets);
 
 app.use(function(req, res){
   //400s before 500s
@@ -57,7 +54,15 @@ app.use(function(req, res){
 
 app.use(function(err, req, res, next){
 //4 arguments creates an error handling middleware
-  console.log('ERRRR', err.stack);
+  var client = require('./lib/loggly')('incoming');
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method,
+    err: err
+  });
   res.status(500).send('my bad');
 })
 var server = app.listen(3000, function () {
